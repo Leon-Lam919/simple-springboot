2471f1e4022167981ea065605bd891bc123507d3
Leon-Lam919 <leonlam34@gmail.com>
2025-01-11 23:50:43 -0600

    Added border to every message sent

--- src/main/resources/static/js/stomp.js
+++ src/main/resources/static/js/stomp.js
@@ -1,16 +1,27 @@
-// Establish WebSocket connection
 var socket = new SockJS('/ws');
 var stompClient = Stomp.over(socket);
 
 stompClient.connect({}, function(frame) {
 	console.log('Connected: ' + frame);
 
-	// Subscribe to a topic
-	stompClient.subscribe('/topic/messages', function(message) {
-		console.log(JSON.parse(message.body).content);
+	// Subscribe to topic
+	stompClient.subscribe('/topic/greeting', function(message) {
+		console.log("Raw message: ", message);
+		const body = JSON.parse(message.body);
+		const msgDiv = document.getElementById('messages'); // Use correct div for output
+		console.log("Message received: ", body.content);
+		msgDiv.innerHTML += `<p style="padding: 3px; margin: 5px;">Message received: ${body.content}</p>`;
 	});
-
-	// Send a message to the server
-	stompClient.send('/app/chat', {}, JSON.stringify({ 'content': 'Hello World!' }));
 });
 
+function sendMessage() {
+	const inputField = document.getElementById('fname');
+	const messageContent = inputField.value.trim(); // Get input value and trim spaces
+
+	if (messageContent) {
+		stompClient.send('/app/hello', {}, JSON.stringify({ name: messageContent }));
+		inputField.value = ""; // Clear input after sending
+	} else {
+		console.log("Cannot send an empty message");
+	}
+}
